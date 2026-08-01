bb1d2b911b9c954a030f3e176f47c354ee92dd00
Prince Pathak <princepathakisme.987@gmail.com>
2026-08-01 15:45:15 +0530

    Refactor comments for section fetching functions
    Updated comments to improve clarity for section fetching functions.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 // calling back function to render hero section
 renderHeroSection();
+
 // calling back function to fetch the trending section
 fetchTrending();
-//calling back function to fetch the popular section
+
+// calling back function to fetch the popular section
 fetchPopular();
-//calling back function to fetch the top rated section
+
+// calling back function to fetch the top rated section
 fetchTopRated();
-//calling back function to fetch the new releases section
+
+// calling back function to fetch the new releases section
 fetchNewReleases();
-//calling back function to fetch genres
+
+// calling back function to fetch genres
 fetchGenres();
+
 // calling back function to render genres section
 renderGenres();
