253aa0f6c347dd638b101fe9bab558ed909d3374
Prince Pathak <princepathakisme.987@gmail.com>
2026-08-05 01:19:36 +0530

    refractor : added comments in hero.js

--- js/hero.js
+++ js/hero.js
@@ -82,6 +82,8 @@ async function fetchTrailer(movieId) {
     const trailer = data.results.find(video => video.type === "Trailer" && video.site === "YouTube");
     return trailer ? `https://www.youtube.com/watch?v=${trailer.key}` : null;
 }
+
+// adding event listeners to open modal and trailer of the movie 
 hero.infoBtn.addEventListener("click", async() => {
     if(!featuredMovie) return;
     openModal(featuredMovie);
